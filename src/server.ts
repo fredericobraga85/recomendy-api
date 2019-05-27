34d74be1e131198c645367921e6f8b8f1be5cc62
Frederico Braga <frederico.braga@x-team.com>
2019-05-26 23:04:35 -0300

    hello world, graphql, typescript

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import * as express from 'express'
+import * as graphqlHTTP from 'express-graphql'
+import { buildSchema } from 'graphql'
+
+const schema = buildSchema(`
+  type Query {
+    hello :String
+  }
+`)
+const root = { hello: () => 'Hello world 1222' }
+const app = express()
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+)
+
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'))
